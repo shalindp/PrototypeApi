fcb8b27a0b5d29cbd03f63f5d1e289ad9fa9de16
Dylan <shalindperer@proton.me>
2023-12-03 11:57:49 +1300

    ext

---
+++ Persistence/Migrations/20231202213805_sort_orders.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class sort_orders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "Occupations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "Interests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "Genders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "Occupations");
+
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "Interests");
+
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "Genders");
+        }
+    }
+}
